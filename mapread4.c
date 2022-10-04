85a1a88b4b7436629863d23960b090c1603079a6
Bilal Ozgur <bozgur@k1m40s05.42istanbul.com.tr>
2022-10-04 12:48:25 +0300

    changed mapread4

---
+++ mapread4.c
@@ -0,0 +1,113 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mapread4.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aozcelik <42istanbul.com.tr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/03 16:01:13 by aozcelik          #+#    #+#             */
+/*   Updated: 2022/10/03 16:01:14 by aozcelik         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./lib/cub3D.h"
+
+int	frgbtrans(char **rgbno)
+{
+	char	*frgb;
+	int		rgbnu;
+
+	frgb = ft_strjoin(rgbno[0], rgbno[1], rgbno[2]);
+	rgbnu = ft_atoi(frgb);
+	return (rgbnu);
+}
+
+int	crgbtrans(char **rgbno)
+{
+	char	*crgb;
+	int		rgbnu;
+
+	crgb = ft_strjoin(rgbno[0], rgbno[1], rgbno[2]);
+	rgbnu = ft_atoi(crgb);
+	return (rgbnu);
+}
+
+static int	g_maptointmap(t_proc *proc)
+{
+	int	i;
+	int	j;
+	int	idx;
+	int	tab;
+
+	i = 0;
+	j = 0;
+	idx = 0;
+	tab = 0;
+	while (proc->g_map.map[i])
+	{
+		if (j > idx)
+			idx = j;
+		if (i > j)
+			idx = i;
+		j = 0;
+		while (proc->g_map.map[i][j])
+		{
+			if (proc->g_map.map[i][j] == '\t')
+				tab += 8;
+			j++;
+		}
+		i++;
+	}
+	return (idx + tab);
+}
+
+static void	set2zero(int *map_line, int idx)
+{
+	int		*tmp;
+
+	map_line = (int *)malloc(sizeof(int) * (idx + 1));
+	tmp = map_line;
+	while (*tmp)
+		ft_memset(tmp++, 0, idx);
+}	
+
+t_proc	*g_mapexportintmap(t_proc *proc)
+{	
+	int		idx;
+	int		i;
+	int		j;
+	int		**imap;
+	char	**cmap;
+
+	i = 0;
+	j = 0;
+	imap = 0;
+	cmap = proc->g_map.map;
+	idx = g_maptointmap(proc);
+	while (cmap[i])
+		i++;
+	imap = (int **)malloc(sizeof(int *) * (idx + 1));
+	if (!imap)
+		return (0);
+	while (i--)
+		set2zero(imap[j++], idx);
+	while (cmap[i][j])
+	{
+		j = 0;
+		while (cmap[i][j])
+		{
+			if (cmap[i][j] == '1' || cmap[i][j] != '0')
+				idx = ft_atoic(cmap[i][j]);
+			else if (cmap[i][j] == 'N' || cmap[i][j] == 'S' \
+					|| cmap[i][j] == 'W' || cmap[i][j] == 'E')
+				;
+			else if (cmap[i][j] == ' ')
+				idx = 0;
+			imap[i][j] = idx;
+			j++;
+		}
+		i++;
+	}
+	proc->g_map.mapi = imap;
+	return (proc);
+}
